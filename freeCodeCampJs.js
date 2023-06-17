6f5401650bf5837111c469cf22945719d823fc9b
ArslanRozyjumayev <136356277+ArslanRozyjumayev@users.noreply.github.com>
2023-06-17 23:33:22 +0300

    Update freeCodeCampJs.js

--- freeCodeCampJs.js
+++ freeCodeCampJs.js
@@ -9,3 +9,23 @@ myStr += "This is the second sentence."
 const myName = "Arslan";
 const myStr = "My name is " + myName + "and I am well!";
 
+// Appending Variables to Strings
+const someAdjective = "wonderful";
+let myStr = "Learning to code is ";
+
+myStr += someAdjective;
+
+// .length
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// .length
+lastNameLength = lastName.length;
+
+
+/*Use Bracket Notation to Find the First Character in a String
+Bracket notation is a way to get a character at */
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+firstLetterOfLastName = lastName[0]; // Change this line
+
