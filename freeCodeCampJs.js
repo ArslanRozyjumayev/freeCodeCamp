481c9e3db1b25822239de2a7b9faa46def149add
ArslanRozyjumayev <136356277+ArslanRozyjumayev@users.noreply.github.com>
2023-06-17 22:11:02 +0300

    Create freeCodeCampJs.js

---
+++ freeCodeCampJs.js
@@ -0,0 +1,11 @@
+const myStr = "This is the start. " + "This is the end."; // String Concatenation
+
+
+let myStr = "This is the first sentence. "; // String Concatenation with compound assignment
+myStr += "This is the second sentence."
+
+
+// Constructing Strings with Variables
+const myName = "Arslan";
+const myStr = "My name is " + myName + "and I am well!";
+
